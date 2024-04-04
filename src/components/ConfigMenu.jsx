6843cf622df9850b8da93eb007ea582d5fd9d2c5
Benjamin Hahl <contact@hahl.media>
2024-04-04 11:11:17 +0200

    configMenu untill radio buttons

--- src/components/ConfigMenu.jsx
+++ src/components/ConfigMenu.jsx
@@ -4,12 +4,15 @@ import jsonData from '../data/productConfig.json';
 export default function ConfigMenu() {
 	const [data, setData] = useState([]);
 	const [subMenuIndex, setSubMenuIndex] = useState([]);
+	const [currentOption, setCurrentOption] = useState([]);
+
 	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		// Simulating asynchronous data fetching with setTimeout
 		setTimeout(() => {
 			setData(jsonData);
 			setSubMenuIndex(0);
+			setCurrentOption('Woodcolor');
 			setIsLoading(false);
 		}, 1000); // Change the delay according to your preference
 	}, []);
@@ -34,9 +37,25 @@ export default function ConfigMenu() {
 			</div>
 			<div>{subMenuIndex}</div>
 			{console.log(data.subMenu[subMenuIndex])}
-			{data[data.subMenu[subMenuIndex]].map((option, index) => (
-				<button key={option}>{option}</button>
+			{data[data.subMenu[subMenuIndex]].map((option) => (
+				<button
+					disabled={currentOption === option}
+					key={option}
+					onClick={() => setCurrentOption(option)}
+				>
+					{option}
+				</button>
 			))}
+			<div>
+				{console.log(data[currentOption])}
+				<p>{currentOption}</p>
+				{data[currentOption].map((item) => (
+					<label key={item}>
+						<input type="radio"></input>
+						{item}
+					</label>
+				))}
+			</div>
 		</div>
 	);
 }
