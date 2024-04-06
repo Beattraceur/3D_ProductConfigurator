9c417921a3873221736b6784662d984d24e400ed
Benjamin Hahl <contact@hahl.media>
2024-04-06 23:21:20 +0200

    boat fully submerged in water

--- src/components/Ocean.jsx
+++ src/components/Ocean.jsx
@@ -6,7 +6,28 @@ export default function Ocean() {
 	const gl = useThree((state) => state.gl);
 	const waterNormals = useLoader(THREE.TextureLoader, '/waternormals.jpeg');
 	waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping;
-	const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), []);
+	const x = 0,
+		y = 0;
+
+	// Create the outer square shape
+	const squareShape = new THREE.Shape();
+	squareShape.moveTo(x - 2000, y - 2000);
+	squareShape.lineTo(x + 2000, y - 2000);
+	squareShape.lineTo(x + 2000, y + 2000);
+	squareShape.lineTo(x - 2000, y + 2000);
+	squareShape.lineTo(x - 2000, y - 2000);
+
+	// Create the bezier curve cutout shape
+	const cutoutShape = new THREE.Shape();
+	cutoutShape.moveTo(x + 13.2, y);
+	cutoutShape.bezierCurveTo(x + 1.7, y - 8.5, x - 11.3, y - 4, x - 11.3, y - 4);
+	cutoutShape.lineTo(x - 11.3, y + 4);
+	cutoutShape.bezierCurveTo(x - 11.3, y + 4, x + 1.7, y + 8.5, x + 13.2, y);
+	cutoutShape.lineTo(x + 13.2, y);
+
+	// Perform Boolean subtraction to create the cutout
+	squareShape.holes.push(cutoutShape);
+	const geom = useMemo(() => new THREE.ShapeGeometry(squareShape), []);
 	const config = useMemo(
 		() => ({
 			textureWidth: 512,
--- src/components/ProductStage.jsx
+++ src/components/ProductStage.jsx
@@ -2,7 +2,7 @@ import { Canvas, extend } from '@react-three/fiber';
 import React, { Suspense } from 'react';
 import StageControl from './StageControl.jsx';
 import BasicDinghy from './BasicDinghy.jsx';
-import { Environment, OrbitControls, Sky } from '@react-three/drei';
+import { Environment, Mask, OrbitControls, Sky } from '@react-three/drei';
 import { Water } from 'three-stdlib';
 import Ocean from './Ocean.jsx';
 
@@ -12,17 +12,20 @@ export default function ProductStage() {
 		<>
 			<Canvas
 				className="Stage"
-				camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}
+				camera={{ position: [-33, 33, -33], fov: 55, near: 1, far: 20000 }}
 			>
-				<pointLight position={[100, 100, 100]} />
-				<pointLight position={[-100, -100, -100]} />
-				<ambientLight />
-				<directionalLight position={[0, 20, 0]} />
+				{/* <pointLight position={[100, 100, 100]} />
+				<pointLight position={[-100, -100, -100]} /> */}
+				<ambientLight intensity={1.5} />
+				<directionalLight position={[600, 150, 500]} intensity={2} />
 				<Suspense fallback={null}>
 					<Ocean />
 					<BasicDinghy />
 				</Suspense>
-				<Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
+				{/* <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} /> */}
+				<Sky scale={1000} sunPosition={[600, 150, 500]} turbidity={0.1} />
+				{/* <Environment preset="dawn" /> */}
+
 				<OrbitControls />
 			</Canvas>
 		</>
